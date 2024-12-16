db3c45e62d7cb79744590ff51e6ba6d87f18895d
cortinovis.21123 <cortinovis.matteo.studente88@itispaleocapa.it>
2024-12-16 13:42:56 +0100

    creazione fxml

--- src/main/java/com/example/viaggi/HelloController.java
+++ src/main/java/com/example/viaggi/HelloController.java
@@ -2,13 +2,47 @@ package com.example.viaggi;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    private agenziaViaggi box=new agenziaViaggi();
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField destinazione;
+    @FXML
+    private TextField costo;
+    @FXML
+    private TextField penscomp;
+    @FXML
+    private TextField andataeritorno;
+    @FXML
+    private TextField escursioni;
+    @FXML
+    private TextField durata;
+    @FXML
+    private Label labellina;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void aggiunta() {
+        if(!destinazione.getText().equals("") && !costo.getText().equals("") && !durata.getText().equals("") &&
+                !penscomp.getText().equals("") && andataeritorno.getText().equals("") && escursioni.getText().equals("")){
+            viaggioPasti v=new viaggioPasti(destinazione.getText(), Integer.parseInt(durata.getText()), Integer.parseInt(costo.getText()), Boolean.parseBoolean(penscomp.getText()));
+            box.inserisci(v);
+            labellina.setText("aggiunto viaggio");
+        }else if(!destinazione.getText().equals("") && !costo.getText().equals("") && !durata.getText().equals("") &&
+                penscomp.getText().equals("") && !andataeritorno.getText().equals("") && escursioni.getText().equals("")){
+            viaggioVolo v=new viaggioVolo(destinazione.getText(), Integer.parseInt(durata.getText()), Integer.parseInt(costo.getText()), Boolean.parseBoolean(andataeritorno.getText()));
+            box.inserisci(v);
+            labellina.setText("aggiunto viaggio");
+        }else if(!destinazione.getText().equals("") && !costo.getText().equals("") && !durata.getText().equals("") &&
+                !penscomp.getText().equals("") && andataeritorno.getText().equals("") && !escursioni.getText().equals("")){
+            viaggioEscursioni v=new viaggioEscursioni(destinazione.getText(), Integer.parseInt(durata.getText()), Integer.parseInt(costo.getText()), Boolean.parseBoolean(penscomp.getText()), Integer.parseInt(escursioni.getText()));
+            box.inserisci(v);
+            labellina.setText("aggiunto viaggio");
+        }else{
+            labellina.setText("parametri inserini erroneamente");
+        }
     }
+
 }
